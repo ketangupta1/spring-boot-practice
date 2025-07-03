8e8ecc99e092dbaf73b69a54e2b984e6b460a7f2
Ketan Gupta <ketankumarnna@gmail.com>
2025-07-03 20:16:48 +0530

    using lombok for simplifying the getter and setter method

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,8 +11,12 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
+//		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var user = new User();
+		user.setName("Ketan");
+		user.setEmail("ketankumar@gmail.com");
+		user.setPassword("password");
+		System.out.println(user);
 	}
 
 }
--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -1,51 +1,25 @@
 package com.codewithmosh.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "email", nullable = false)
     private String email;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     @Column(name = "password", nullable = false)
     private String password;
