a68ff5e7361e422861ed954d98b272219528d5b3
Ketan Gupta <ketankumarnna@gmail.com>
2025-07-04 20:57:13 +0530

    save delete and find using repository

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store;
 
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,13 +12,21 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var repository = context.getBean(UserRepository.class);
+
 		var user = User.builder()
 				.name("Ketan")
 				.email("ketan.kumar@gmail.com")
 				.password("password")
 				.build();
-		System.out.println(user);
+
+		repository.save(user);
+		var user1 = repository.findById(3L).orElseThrow();
+
+		System.out.println(user1);
+
+		repository.delete(user1);
 	}
 
 }
